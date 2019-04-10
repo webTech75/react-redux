ebe17e116665d0e532e86fdbf315290d45ec8deb
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-04-10 11:35:06 -0500

    add contact and home components

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Contact(props) {
+    return(
+        <div className="container">
+            <div className="row row-content">
+                <div className="col-12">
+                <h3>Location Information</h3>
+                </div>
+                <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                        121, Street St.<br />
+                       City Name, State<br />
+                        USA<br />
+                        <i className="fa fa-phone"></i>: +1234567890<br />
+                        <i className="fa fa-fax"></i>: +1234567890<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:amrelhabbal1@gmail.com">amrelhabbal1@gmail.com</a>
+                        </address>
+                </div>
+                <div className="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                </div>
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary" href="tel:+1234567890"><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success" href="mailto:amrelhabbal1@gmail.com"><i className="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import { DISHES } from '../helper/dishes';
+import ContactComponent from './ContactComponent';
 import {DishDetails} from './DishDetails';
+import { COMMENTS } from '../helper/comments';
+import { PROMOTIONS } from '../helper/promotions';
+import { LEADERS } from '../helper/leaders';
 import { Footer } from './FooterComponent';
-import { Home } from './HomeComponent';
+import  Home  from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class MainComponent extends Component {
 
   state = {
     dishes : DISHES,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS,
     selectedDish: null
   }
 
@@ -24,14 +31,20 @@ class MainComponent extends Component {
             <DishDetails dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />*/
   render() {
     const HomePage = () => (
-      <Home />
-    )
+      <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
+    );
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={ContactComponent} />
           <Redirect to='/home' />
         </Switch>
 
