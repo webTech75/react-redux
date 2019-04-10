3c771c0bff8d5e1d62b3b76cd160d8243b54bc17
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-04-10 13:06:51 -0500

    add route param

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,18 +1,60 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-export const DishDetails = ({dish}) => {
+
+
+const RenderDish = ({dish}) => (
+    <div className="col-12 col-md-6">
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    </div>
+)
+
+const RenderComments = ({comments}) => (
+    comments != null ? (
+        <div classname="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {
+                    comments.map((comment) => (
+                        <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.auther} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    ):(
+        <div></div>
+    )
+)
+
+export const DishDetails = (props) => {
     return (
-        dish ? (
-            <div className="row">
-                <div className="col-12 col-md-6">
-                    <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                    </Card>
+        props.dish != null ? (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/menu'>Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                   <RenderDish dish={props.dish} />
+                   <RenderComments comments={props.comment} />
                 </div>
             </div>
          ): (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -38,12 +38,19 @@ class MainComponent extends Component {
       />
     );
 
+    const DishWithId = ({match}) => (
+      <DishDetails dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comment={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+      />
+    )
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={ContactComponent} />
           <Redirect to='/home' />
         </Switch>
