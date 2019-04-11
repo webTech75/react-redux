b86faff20c2c9ba30e7c031edffc01a1469d0847
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-04-11 16:15:38 -0500

    add google redux devtools extension

--- src/container/configStore.js
+++ src/container/configStore.js
@@ -2,7 +2,11 @@ import { createStore } from 'redux';
 import { Reducer, initialState } from './reducer';
 
 export const ConfigStore = () => {
-   const store = createStore(Reducer, initialState);
+   const store = createStore(
+      Reducer,
+      initialState,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      );
 
    return store;
 }
