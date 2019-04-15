572262959b0e7eccc518d24b7a4ad0a226d86c1d
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-04-15 16:45:28 -0500

    fix typo

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -39,7 +39,7 @@ export const Footer = () => (
        </div>
        <div className="row justify-content-center">
            <div className="col-auto">
-               <p>© Copyright 2019 Ristorant</p>
+               <p>© Copyright 2019 Ristorante</p>
            </div>
        </div>
    </div>
