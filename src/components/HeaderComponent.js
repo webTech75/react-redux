439e93c98808f4c42cd07c8956151672aa88626d
Amr Elhabbal <amrelhabbal1@gmail.com>
2019-04-15 16:17:17 -0500

    fix typo

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -70,7 +70,7 @@ export default class Header extends Component {
                <div className="container">
                   <div className="row row-header">
                      <div className="col-12 col-sm-6">
-                        <h1>RISTORANT</h1>
+                        <h1>RISTORANTE</h1>
                         <p>We take inspiration from the world's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                      </div>
                   </div>
